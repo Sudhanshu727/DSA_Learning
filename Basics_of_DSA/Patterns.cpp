a57f28391d9fc3e2a66e07de51c136b5f70c44a6
Sudhanshu Shekhar <sudhanshus7907@gmail.com>
2025-03-13 20:44:45 +0530

    Update Patterns.cpp

--- Basics_of_DSA/Patterns.cpp
+++ Basics_of_DSA/Patterns.cpp
@@ -53,6 +53,48 @@ void triangle(int n)
     }
 }
 
+void seeding(int n)
+{
+    // Write your code here.
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void nNumberTriangle(int n)
+{
+    // Write your code here.
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << i - j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void nStarTriangle(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
@@ -64,6 +106,12 @@ int main()
     nTriangle(n);
     cout << "\n";
     triangle(n);
+    cout << "\n";
+    seeding(n);
+    cout << "\n";
+    nNumberTriangle(n);
+    cout << "\n";
+    nStarTriangle(n);
     cout << "\n";
     return 0;
 }
