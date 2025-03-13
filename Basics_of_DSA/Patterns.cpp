1112517f40c2b358c8fe5ee6d3a54c115a329bb0
Sudhanshu Shekhar <sudhanshus7907@gmail.com>
2025-03-13 21:37:45 +0530

    Update Patterns.cpp

--- Basics_of_DSA/Patterns.cpp
+++ Basics_of_DSA/Patterns.cpp
@@ -94,6 +94,115 @@ void nStarTriangle(int n)
         cout << endl;
     }
 }
+void nRevStarTriangle(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 2 * (n - i) + 1; k > 0; k--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void nStarDiamond(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 2 * (n - i) + 1; k > 0; k--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void nRtStarTriangle(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = n; i >= 1; i--)
+    {
+        for (int k = i - 1; k > 0; k--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void nBinaryTriangle(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << 1 << " ";
+            }
+            else
+            {
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+void numberCrown(int n)
+{
+    // Write your code here.
+    // number
+    int sp = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+        // number
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << "";
+        }
+        // space
+        for (int j = 1; j <= sp; j++)
+        {
+            cout << " ";
+        }
+        // numbers in reverse order
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << "";
+        }
+        cout << endl;
+        sp = sp - 2;
+    }
+}
 
 int main()
 {
@@ -112,6 +221,16 @@ int main()
     nNumberTriangle(n);
     cout << "\n";
     nStarTriangle(n);
+    cout << "\n";
+    nRevStarTriangle(n);
+    cout << "\n";
+    nStarDiamond(n);
+    cout << "\n";
+    nRtriangle(n);
+    cout << "\n";
+    nBinaryTriangle(n);
+    cout << "\n";
+    numberCrown(n);
     cout << "\n";
     return 0;
 }
