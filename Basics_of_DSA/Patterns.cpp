35d51749f9b347c74014b45e4dc2b70fa92f2ad0
Sudhanshu Shekhar <sudhanshus7907@gmail.com>
2025-03-13 13:34:09 +0530

    Triangle Patterns

--- Basics_of_DSA/Patterns.cpp
+++ Basics_of_DSA/Patterns.cpp
@@ -1,8 +1,69 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+void nForest(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void nRtriangle(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    // Write your code here.
+}
+
+void nTriangle(int n)
+{
+    // Write your code here
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void triangle(int n)
+{
+    // Write your code here
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
     int n;
-    cin>> n;
+    cin >> n;
+    nForest(n);
+    cout << "\n";
+    nRtriangle(n);
+    cout << "\n";
+    nTriangle(n);
+    cout << "\n";
+    triangle(n);
+    cout << "\n";
     return 0;
 }
