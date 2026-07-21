1775541fa50cb2676ff599999b4c55174b9cecf6
Sudhanshu Shekhar <sudhanshus7907@gmail.com>
2026-07-22 00:37:27 +0530

    Time: 0 ms (100%), Space: 59.2 MB (38.82%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        int product = k * threshold;
-        int sum = 0;
-        int ans = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            if (i >= k) {
-                sum -= arr[i-k];
-            }
-            sum += arr[i];
-            if (i >= k-1) {
-                ans += (sum >= product);
-            }
+        int comp = threshold*k;
+        int sum=0, n= arr.size();
+        for(int i=0; i<k; i++){
+            sum+= arr[i];
+        }
+        int ans= (sum>=comp);
+
+        for(int i=0, j=k; j<n; j++, i++){
+            sum-= arr[i];
+            sum+= arr[j];
+
+            ans+= (sum>=comp);
         }
         return ans;
     }
