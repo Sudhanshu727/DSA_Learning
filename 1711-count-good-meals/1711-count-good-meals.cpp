baae72670a427047430ea12be4f3ded2bd2830d0
Sudhanshu Shekhar <sudhanshus7907@gmail.com>
2026-07-24 00:51:04 +0530

    Time: 719 ms (13.23%), Space: 199.9 MB (20.24%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int countPairs(vector<int>& deliciousness) {
         unordered_map<int,int>freq;
-        vector<int> power(22,1);
+        vector<int> power(22);
         int ans=0;
-        for(int i=1; i<22;i++){
+        for(int i=0; i<22;i++){
             power[i]= 1<<i;
         }
         int mod= 7+1e9;
